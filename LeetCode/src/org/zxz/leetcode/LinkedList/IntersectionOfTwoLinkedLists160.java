61b7bd90fc921060f9aaab28de17e4cfb8130c16
zxz <2646459516@qq.com>
2022-08-12 19:04:22 +0800

    8/12 160

--- LeetCode/src/org/zxz/leetcode/LinkedList/IntersectionOfTwoLinkedLists160.java
+++ LeetCode/src/org/zxz/leetcode/LinkedList/IntersectionOfTwoLinkedLists160.java
@@ -9,20 +9,28 @@ public class IntersectionOfTwoLinkedLists160 {
     public static void main(String[] args) {
         ListNode headA = new ListNode(4);
         headA.add(1);
-        headA.add(8);
-        headA.add(4);
-        headA.add(5);
         ListNode headB = new ListNode(5);
         headB.add(6);
         headB.add(1);
-        headB.add(8);
-        headB.add(4);
-        headB.add(5);
+        ListNode intersection = new ListNode(8);
+        intersection.add(4);
+        intersection.add(5);
+        headA.add(intersection);
+        headB.add(intersection);
         IntersectionOfTwoLinkedLists160 intersectionOfTwoLinkedLists160 = new IntersectionOfTwoLinkedLists160();
         ListNode node = intersectionOfTwoLinkedLists160.getIntersectionNode(headA, headB);
-        System.out.println(node.val);
+        System.out.println(node == null ? "null" : node.val);
     }
 
+    /**
+     * 双指针
+     * slow: 4->1->8->4->5->null->5->6->1->8->4->5->null
+     * fast: 5->6->1->8->4->5->null->4->1->8->4->5->null
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
     private ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
             return null;
@@ -33,5 +41,41 @@ public class IntersectionOfTwoLinkedLists160 {
             fast = fast == null ? headA : fast.next;
         }
         return slow;
+    }
+
+    /**
+     * @param headA
+     * @param headB
+     * @return
+     */
+    private ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        int lenA = getLength(headA);
+        int lenB = getLength(headB);
+        ListNode longHead = lenA > lenB ? headA : headB;
+        ListNode shortHead = lenA > lenB ? headB : headA;
+        int diff = Math.abs(lenA - lenB);
+        for (int i = 0; i < diff; i++) {
+            longHead = longHead.next;
+        }
+        while (longHead != null && shortHead != null) {
+            if (longHead == shortHead) {
+                return longHead;
+            }
+            longHead = longHead.next;
+            shortHead = shortHead.next;
+        }
+        return null;
+    }
+
+    private int getLength(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
     }
 }
--- LeetCode/src/org/zxz/leetcode/LinkedList/ListNode.java
+++ LeetCode/src/org/zxz/leetcode/LinkedList/ListNode.java
@@ -1,5 +1,8 @@
 package org.zxz.leetcode.LinkedList;
 
+/**
+ * @author LEGION
+ */
 public class ListNode {
     int val;
     ListNode next;
@@ -19,6 +22,19 @@ public class ListNode {
     //尾插法
     public ListNode add(int val) {
         ListNode node = new ListNode(val);
+        if (this.next == null) {
+            this.next = node;
+        } else {
+            ListNode p = this;
+            while (p.next != null) {
+                p = p.next;
+            }
+            p.next = node;
+        }
+        return this;
+    }
+
+    public ListNode add(ListNode node) {
         if (this.next == null) {
             this.next = node;
         } else {
